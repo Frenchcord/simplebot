6a8e7fc6f4940a2716f806b32e303301a4b1e2da
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-06-28 10:13:59 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import frenchcord
 client = frenchcord.Robot('bot_token')
-@client.event('en_marche')
+@client.event('on_ready')
 def say_online():
   print('online!')
+
+@client.event('message')
+def cmd(message):
+  if message.author.id == client.me.id: return
+  client.process_commands(message) 
  
 @client.command
 def hello(ctx):
